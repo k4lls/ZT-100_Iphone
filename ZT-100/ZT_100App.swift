394960b92f32e02cb3b12b7b7a076dba65ba31ae
k4lls <materre@gmail.com>
2025-12-11 10:44:23 +0100

    Initial Commit

---
+++ ZT-100/ZT_100App.swift
@@ -0,0 +1,17 @@
+//
+//  ZT_100App.swift
+//  ZT-100
+//
+//  Created by Admin on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ZT_100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
